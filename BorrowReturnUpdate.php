cf983c6a1eefd8596accdc2b9b9da154c423655a
Dilushan <2019e032@eng.jfn.ac.lk>
2022-11-15 23:41:47 +0530

    dilu

---
+++ BorrowReturnUpdate.php
@@ -0,0 +1,15 @@
+<?php
+	//connect with mysql
+	$con = mysqli_connect('localhost','root','');
+	// select database
+	mysqli_select_db($con, 'library_database');
+	//SELECT QUERY
+    $sql = "INSERT INTO return_book(rtn_id,std_id,book_id,book_title,rtn_date)
+     Values('$_POST[BorrowID]','$_POST[StudentID]','$_POST[BookID]','$_POST[BookTitle]','$_POST[BorrowDate]')";
+    //Execute the query
+    if(mysqli_query($con,$sql))
+    	header("refresh:1; url=BorrowBookReturn.php");
+    else
+    	echo "Not Updated";
+
+?>
